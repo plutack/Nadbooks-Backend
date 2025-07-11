c1fdeed355643d934be43d12b49a804715583e8a
Talut Salako <salakotalut@gmail.com>
2025-07-11 09:52:04 +0100

    update env sample file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from '@/auth/auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
+	imports: [
+		PrismaModule,
+		JwtModule.registerAsync({
+			useFactory: (config: ConfigService) => ({
+				secret: config.get('JWT_SECRET'),
+				signOptions: { expiresIn: config.get('JWT_EXPIRES_IN') },
+			}),
+			inject: [ConfigService],
+		}),
+	],
+	controllers: [AuthController],
 	providers: [AuthService],
+	exports: [AuthService],
 })
 export class AuthModule {}
