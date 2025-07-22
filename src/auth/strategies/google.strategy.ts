faa4e00d6706a103c807caefb981af6af32c8565
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-07-22 13:27:51 +0100

    handle missing values

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -10,10 +10,22 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         private config: ConfigService,
         private prismaService: PrismaService
     ) {
+        const GOOGLE_CLIENT_ID = config.get<string>("GOOGLE_CLIENT_ID")
+        const GOOGLE_CLIENT_SECRET = config.get<string>("GOOGLE_CLIENT_SECRET")
+        const GOOGLE_CALLBACK_URL = config.get<string>("GOOGLE_CALLBACK_URL")
+        if (!GOOGLE_CLIENT_SECRET) {
+            throw new Error("GOOGLE_CLIENT_SECRET not provided")
+        }
+        if (!GOOGLE_CALLBACK_URL) {
+            throw new Error("GOOGLE_CALLBACK_URL not provided")
+        }
+        if (!GOOGLE_CLIENT_ID) {
+            throw new Error("GOOGLE_CLIENT_ID not provided")
+        }
         super({
-            clientID: config.get("GOOGLE_CLIENT_ID") || "",
-            clientSecret:config.get("GOOGLE_CLIENT_SECRET") || "",
-            callbackURL: config.get("GOOGLE_CALLBACK_URL") || "",
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret:GOOGLE_CLIENT_SECRET,
+            callbackURL: GOOGLE_CALLBACK_URL,
             scope: ["profile", "email"]
         });
     }
