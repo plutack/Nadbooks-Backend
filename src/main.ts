484754c9715c8fb0ae3b9e1719e79227cbd01439
Talut Salako <salakotalut@gmail.com>
2025-07-26 15:25:32 +0100

    initialize swagger to /docs route

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import metadata from '@/metadata';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -9,6 +11,15 @@ async function bootstrap() {
 	app.use(cookieParser());
 	app.setGlobalPrefix("api")
 	const config = app.get(ConfigService);
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('Nadbooks-Backend')
+		.setDescription(`nadbooks backend API specification `)
+		.setVersion('0.1')
+		.build();
+
+	await SwaggerModule.loadPluginMetadata(metadata);
+	const apiDoc = () => SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('docs', app, apiDoc);
 	await app.listen(config.get('PORT') ?? 3000);
 }
 bootstrap();
