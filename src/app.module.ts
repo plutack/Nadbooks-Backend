e692e4df2ef7f0059c1cd38070a3ed210eeb2e04
Talut Salako <salakotalut@gmail.com>
2025-08-15 15:19:06 +0100

    implement middleware logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { AuthModule } from '@/auth/auth.module';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { BooksModule } from './books/books.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
 	imports: [
@@ -18,4 +19,10 @@ import { BooksModule } from './books/books.module';
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+			.apply(LoggerMiddleware)
+			.forRoutes({ path: '*', method: RequestMethod.ALL });
+	}
+}
