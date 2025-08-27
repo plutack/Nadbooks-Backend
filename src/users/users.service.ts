64e50c5a14be0b09433e5e76da1a0a72d934d908
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-08-27 08:30:37 +0100

    define user service class

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { PrismaService } from '@/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+    constructor(
+        private readonly db: PrismaService
+    ) { }
+
+    userBookmarks(user: JwtPayloadType) {
+        return this.db.bookBookmark.findMany({
+            where: {
+                userId: user.sub
+            },
+            select: {
+                book: true,
+                bookId: false,
+                user: false,
+                userId: false
+            }
+        })
+    }
+}
