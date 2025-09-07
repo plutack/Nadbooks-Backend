9304e6c5e53d72508470ac1aff681d7fb13c3db6
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-09-07 08:52:14 +0100

    remove unused imports

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,6 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
 import { CreateUserDto, GoogleAuthDto, LoginUserDto } from '@/auth/dtos/auth.dto';
-import { GoogleOauthGuard } from '@/auth/guards/google-oauth.guard';
 
 @Controller('auth')
 export class AuthController {
