bdf70aff69a5034f350ed5140814da5d595c3ea7
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-09-07 08:52:08 +0100

    update methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from '@/auth/auth.service';
-import { CreateUserDto, LoginUserDto } from '@/auth/dtos/auth.dto';
+import { CreateUserDto, GoogleAuthDto, LoginUserDto } from '@/auth/dtos/auth.dto';
 import { GoogleOauthGuard } from '@/auth/guards/google-oauth.guard';
 
 @Controller('auth')
@@ -18,13 +18,9 @@ export class AuthController {
 		return this.auth.login(body);
 	}
 
-	@Get('google')
-	@UseGuards(GoogleOauthGuard)
-	googleAuth() {}
-
-	@Get('google/callback')
-	@UseGuards(GoogleOauthGuard)
-	async googleAuthCallback(@Req() req: Request) {
-		return await this.auth.registerGoogleUser(req.user as GoogleResponseUser);
+	@Post('google')
+	googleAuth(@Body() body: GoogleAuthDto) {
+		return this.auth.google(body.token)
 	}
+
 }
