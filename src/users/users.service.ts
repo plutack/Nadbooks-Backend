25cf62d9bf3f91ab697c36691661d280e3eb54cd
Talut Salako <salakotalut@gmail.com>
2025-09-21 16:30:06 +0100

    fix formatting

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,31 +1,30 @@
-import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { JwtPayloadType } from '@/types/jwt.type';
 
 @Injectable()
 export class UserService {
-    constructor(
-        private readonly db: PrismaService
-    ) { }
+	constructor(private readonly db: PrismaService) {}
 
-    userBookmarks(user: JwtPayloadType) {
-        return this.db.bookBookmark.findMany({
-            where: {
-                userId: user.sub
-            },
-            select: {
-                book: true,
-                bookId: false,
-                user: false,
-                userId: false
-            }
-        })
-    }
+	userBookmarks(user: JwtPayloadType) {
+		return this.db.bookBookmark.findMany({
+			where: {
+				userId: user.sub,
+			},
+			select: {
+				book: true,
+				bookId: false,
+				user: false,
+				userId: false,
+			},
+		});
+	}
 
-    booksByUser(user: JwtPayloadType) {
-        return this.db.book.findMany({
-            where: {
-                userId: user.sub
-            }
-        })
-    }
+	booksByUser(user: JwtPayloadType) {
+		return this.db.book.findMany({
+			where: {
+				userId: user.sub,
+			},
+		});
+	}
 }
