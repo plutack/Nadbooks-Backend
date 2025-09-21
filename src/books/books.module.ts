d12602df411a6eadf23464605d7ecae87c8746f7
Talut Salako <salakotalut@gmail.com>
2025-09-21 16:20:08 +0100

    centralize JWT module configuration

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,25 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { StorageModule } from '@/storage/storage.module';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
-import { StorageModule } from '@/storage/storage.module';
 
 @Module({
-	imports: [
-		PrismaModule,
-		StorageModule,
-		JwtModule.registerAsync({
-			imports: [ConfigModule],
-			useFactory: (config: ConfigService) => ({
-				secret: config.get('JWT_SECRET'),
-				signOptions: { expiresIn: config.get('JWT_EXPIRES_IN') },
-			}),
-			inject: [ConfigService],
-		}),
-	],
+	imports: [PrismaModule, StorageModule],
 	controllers: [BooksController],
 	providers: [BooksService],
 })
-export class BooksModule { }
+export class BooksModule {}
