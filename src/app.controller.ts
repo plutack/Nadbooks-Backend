16fd3493ac1a7caae0c4097b3963326597dc10b3
SpacePanda7077 <spacepanda7077@gmail.com>
2025-10-14 06:38:24 -0700

    added blockchain module and fixed some changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get()
-	// @UseGuards(JwtGuard)
+	@UseGuards(JwtGuard)
 	getHello(): string {
 		return this.appService.getHello();
 	}
