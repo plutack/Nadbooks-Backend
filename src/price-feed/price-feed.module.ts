607127337163030d08301d2d1d4976144f02a190
Talut Salako <salakotalut@gmail.com>
2025-11-08 18:46:49 +0100

    feat(price-feed): Enhance currency conversion and add   preview functionality`

--- src/price-feed/price-feed.module.ts
+++ src/price-feed/price-feed.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PriceFeedService } from './price-feed.service';
+import { PriceFeedController } from './price-feed.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+	imports: [HttpModule],
 	providers: [PriceFeedService],
 	exports: [PriceFeedService],
+	controllers: [PriceFeedController],
 })
 export class PriceFeedModule {}
