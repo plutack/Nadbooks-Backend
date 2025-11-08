71af230edadd38d4b279214fb40bec59b3364111
Talut Salako <salakotalut@gmail.com>
2025-11-08 18:41:27 +0100

    feat(transactions): Add utility function to generate unique references

---
+++ src/helpers/functions.ts
@@ -0,0 +1,25 @@
+import { TransactionType } from 'generated/prisma';
+
+export function generateRef(type: TransactionType, userId: string): string {
+	const prefix = (() => {
+		switch (type) {
+			case 'DEPOSIT':
+				return 'DEP';
+			case 'WITHDRAWAL':
+				return 'WTH';
+			case 'ORDER':
+				return 'ORD';
+			default:
+				throw new Error('invald transaction type');
+		}
+	})();
+
+	const timestamp = new Date()
+		.toISOString()
+		.replace(/[-:.TZ]/g, '')
+		.slice(0, 14);
+	const unique = Math.random().toString(36).substring(2, 8).toUpperCase();
+	const shortUser = userId.slice(-4).toUpperCase();
+
+	return `${prefix}-${timestamp}-${shortUser}-${unique}`;
+}
