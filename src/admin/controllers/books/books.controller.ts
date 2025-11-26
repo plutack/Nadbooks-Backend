c9047c4990cbc9d821577d4947e84f46fbe7196b
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-11-26 23:43:09 +0100

    feat: add guard

--- src/admin/controllers/books/books.controller.ts
+++ src/admin/controllers/books/books.controller.ts
@@ -1,11 +1,22 @@
 import { AdminEditBookDto } from '@/admin/dto/books/edit-book.dto';
+import { AdminAuthGuard } from '@/admin/guards/admin.guard';
 import { AdminBooksService } from '@/admin/services/books/books.service';
 import { BaseFilterQueryType } from '@/types/filters.type';
-import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+	Body,
+	Controller,
+	Get,
+	Param,
+	Patch,
+	Query,
+	UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('admin/books')
 @ApiTags('Admin Books')
+@Controller('admin/books')
+@ApiBearerAuth()
+@UseGuards(AdminAuthGuard)
 export class AdminBooksController {
 	constructor(private adminBookService: AdminBooksService) {}
 
