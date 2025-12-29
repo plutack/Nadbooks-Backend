7547b32f18f949e1708034b4fe0214a8938554d8
Talut Salako <salakotalut@gmail.com>
2025-12-29 09:05:15 +0100

    refactor: remove redundant function
    Prisma client coupled with the validator already effectively handles the intended action

--- src/helpers/dto/clean-dto.util.ts
+++
@@ -1,5 +0,0 @@
-export function cleanObject<T extends Record<string, any>>(obj: T): Partial<T> {
-	return Object.fromEntries(
-		Object.entries(obj).filter(([_, v]) => v !== undefined),
-	) as Partial<T>;
-}
